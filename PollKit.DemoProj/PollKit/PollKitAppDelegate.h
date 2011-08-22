1b5922aaf5b077de5f12fb0b67d8041170f3e28e
Ali Raza <dejavu87@gmail.com>
2011-08-22 15:56:11 +0500

    my first commit for initial project structure

---
+++ PollKit.DemoProj/PollKit/PollKitAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  PollKitAppDelegate.h
+//  PollKit
+//
+//  Created by Ali Raza on 8/22/11.
+//  Copyright 2011 Welltime Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PollKitAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ PollKit.DemoProj/PollKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PollKit
+//
+//  Created by Ali Raza on 8/22/11.
+//  Copyright 2011 Welltime Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
